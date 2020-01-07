22bcb9af8e90d0cf9e45129802bcbc09170b4a73
shikha agrawal <shikha130vv@outlook.com>
2020-01-07 12:19:43 +0530

    set branch as feature 1.1

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -32,7 +32,7 @@ namespace test.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)],
-                Branch = "feature1"
+                Branch = "feature1.1"
             })
             .ToArray();
         }
