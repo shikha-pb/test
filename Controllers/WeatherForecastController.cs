b95529fd66fcc4c3a55c56dc85169effc8766826
shikha agrawal <shikha130vv@outlook.com>
2020-01-07 11:58:04 +0530

    Merge branch changes in qa and feature2 abd set branch as feature12

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -32,7 +32,7 @@ namespace test.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)],
-                Branch = "feature2"
+                Branch = "feature12"
             })
             .ToArray();
         }
